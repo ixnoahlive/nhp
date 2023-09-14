7a6ff392a2c3cdf7b260915861406d9678626cdd
ixNoah <70314622+NoahTheNerd@users.noreply.github.com>
2023-09-14 22:14:43 +0200

    🌒 Final 14.9 nightly commit handling a sound error. Enjoy!

--- src/packets/sound.js
+++ src/packets/sound.js
@@ -5,6 +5,8 @@ module.exports = {
     requiredArgs: ['name'],
     args: {
         name: 'The name of the sound with dots replaced by space bar (e.g. mob ghast scream)',
+        modernName: 'The name of the sound on 1.20, if this is left blank the mod will use default name',
+        namespace: 'The namespace of the sound on 1.20, if this is left blank it will use minecraft'
 
         volume: 'The volume to play the sound at (capped at 1.5)',
         pitch: 'The pitch to play the sound at (capped at 3)',
@@ -23,5 +25,7 @@ module.exports = {
             World.playSound(args.name.replace(/[ ,]/g, '.').removeFormatting(), 1, 1)
         }, parseFloat(args.delay) ? parseFloat(args.delay) * 1000 : 0);
     },
-    runModern(args) {this.run(args)}
+    runModern(args) {
+        ChatLib.chat('&c[NHP] The Sound packet is unavailable on 1.20.')
+    }
 }
